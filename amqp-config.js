78f049f0d8c6f80b22c60c65aa73a728f1880ded
Robin Lenz <robin.lenz@5minds.de>
2024-09-26 21:32:16 +0200

    get connection error

--- amqp-config.js
+++ amqp-config.js
@@ -4,9 +4,12 @@ module.exports = function(RED) {
     function AMQPServerNode(n) {
         RED.nodes.createNode(this,n);
         this.connectionString = n.connectionString;
+        const node = this;
 
         this.connection = new AMQPConnection(this.connectionString);
-        this.connection.connect();
+        this.connection.connect().catch((error) => {
+            node.error(error);
+        });
     }
     RED.nodes.registerType("amqp-config",AMQPServerNode);
 }
